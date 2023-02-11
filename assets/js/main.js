b7ae792dd35fce106b248cb72971133fab4eede0
heyjoeway <joe@jojudge.com>
2023-02-11 15:22:08 -0500

    Migrate to github-based comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
-
-$(() => {
+addEventListener('DOMContentLoaded', (event) => {
     // Dice roll!
     let htmlDiceEntities = ["&#x2680;","&#x2681;","&#x2682;","&#x2683;","&#x2684;","&#x2685;"];
     
     function randomElement(arr) {
         return arr[Math.round(Math.random()*arr.length)];
     }
-    
-    $("#bg-bottom-right").html(randomElement(htmlDiceEntities));
+
+    let e_bg_bottom_right = document.getElementById("bg-bottom-right");
+    e_bg_bottom_right.innerHTML = randomElement(htmlDiceEntities);    
 });
