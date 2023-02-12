d046d29627eb75212c5934607032eb82a0834fb7
heyjoeway <joe@jojudge.com>
2023-02-11 22:40:04 -0500

    i am ashamed

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ addEventListener('DOMContentLoaded', (event) => {
     let htmlDiceEntities = ["&#x2680;","&#x2681;","&#x2682;","&#x2683;","&#x2684;","&#x2685;"];
     
     function randomElement(arr) {
-        return arr[Math.round(Math.random()*arr.length)];
+        return arr[Math.round(Math.random()*(arr.length - 1))];
     }
 
     let e_bg_bottom_right = document.getElementById("bg-bottom-right");
