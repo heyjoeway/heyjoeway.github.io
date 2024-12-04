236168046a5f40cb9aa14ac03c60ff07ef747815
Joseph <joe@jojudge.com>
2024-12-04 14:57:29 -0500

    WIP tweets

---
+++ src/routes/feeds/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Layout from "$lib/Layout.svelte";
+	import Comments from "$lib/Comments.svelte";
+	
+	export let data;
+</script>
+
+<Layout data={data}>
+
+	<slot></slot>
+	
+    {#await import(`./main/17171656156/*.png`) then { default: src }}
+        <img {src} alt="Image" />
+    {/await}
+    
+	<br>
+
+    <hr>
+    <!-- <Comments pageID={data.id} pageTitle={data.fm.title} /> -->
+</Layout>
