38a67f5696a3a0464de0c1d6962ab8eb33f96f66
heyjoeway <joe@jojudge.com>
2025-02-16 21:00:34 -0500

    The actual homepage!

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,15 @@
-import { getArticles } from '$lib/Article';
+import { getAllFeeds, sortPosts, type Post } from '$lib/Feed.js';
 
 export async function load() {
-	return {
-		articles: await getArticles()
-	};
+	const feeds = await getAllFeeds(true);
+	
+	let posts = (feeds
+		.map(feed => feed.posts) // Get all posts from each feed
+		.filter(x => x) // Remove undefined
+		.flat() // Put all into a single list
+	) as Post[];
+	sortPosts(posts);
+	posts = posts.slice(0, 5);
+	
+	return { posts };
 }
