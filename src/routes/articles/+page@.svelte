890887ef123ebdf5099ed87cd1bb92ddfd6d2ae0
heyjoeway <joe@jojudge.com>
2025-02-17 20:17:23 -0500

    Change some terminology here

--- src/routes/articles/+page@.svelte
+++ src/routes/articles/+page@.svelte
@@ -5,9 +5,9 @@
 	export let data;
 </script>
 <ArticleLayout data={data}>
-	<h1>Pinned</h1>
+	<h1>Pinned Articles</h1>
 	<ArticleFeed pinned={true} articles={data.articles} />
 	<hr>
-	<h1>Recent Posts</h1>
+	<h1>Recent Articles</h1>
 	<ArticleFeed articles={data.articles} />
 </ArticleLayout>
