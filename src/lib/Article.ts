2dfe460615334ee6eb86ce0a598ae3eaadf357a8
heyjoeway <joe@jojudge.com>
2025-03-05 10:56:43 -0500

    Fix article feed not showing correct date

--- src/lib/Article.ts
+++ src/lib/Article.ts
@@ -24,6 +24,12 @@ export interface ArticleLoaded {
 	fm?: ArticleFrontmatter;
 }
 
+interface MdsvexCompiled {
+    code: string;
+    data?: Record<string, unknown>;
+    map?: string;
+}
+
 export async function getAllCategories(): Promise<string[]> {
 	// Get list of all directories in `content/articles`
 	const articleSlugs = (
@@ -35,11 +41,12 @@ export async function getAllCategories(): Promise<string[]> {
 		.map(dirent => dirent.name )
 	);
 	// For each slugs, compile the page.svx file and extract the categories
-	const categorySet = new Set();
+	const categorySet: Set<string> = new Set();
 	await Promise.all(articleSlugs.map(async slug => {
-		const postContents = fs.readFileSync(`src/routes/articles/${slug}/+page.svx`, 'utf-8');
+		const postPath = `src/routes/articles/${slug}/+page.svx`;
+		const postContents = fs.readFileSync(postPath, 'utf-8');
 		const compiled = await compile(postContents);
-		const frontmatter = compiled?.data?.fm as ArticleFrontmatter;
+		const frontmatter = await getArticleFrontmatter(postPath, compiled);
 		frontmatter.categories?.split(" ").forEach(category => categorySet.add(category));
 	}));
 	return Array.from(categorySet);
@@ -60,13 +67,14 @@ export async function getArticles(category?: string): Promise<ArticleLoaded[]> {
 	);
 	
 	await Promise.all(articles.map(async article => {
+		const articlePath = `src/routes/articles/${article.slug}/+page.svx`;
 		const articleRaw = (
 			await util.promisify(fs.readFile)
-				(`src/routes/articles/${article.slug}/+page.svx`, 'utf-8')
+				(articlePath, 'utf-8')
 		) as string;
-		const compiled = await compile(articleRaw);
+		const compiled = await compile(articleRaw) as MdsvexCompiled;
 		article.code = compiled?.code;
-		article.fm = compiled?.data?.fm as ArticleFrontmatter;
+		article.fm = await getArticleFrontmatter(articlePath, compiled);
 	}));
 	
 	if (category) {
@@ -83,10 +91,15 @@ export async function getArticles(category?: string): Promise<ArticleLoaded[]> {
 	return articles;
 }
 
-export async function getArticleFrontmatter(postPath: string): Promise<ArticleFrontmatter> {
-	const postContents = fs.readFileSync(postPath, 'utf-8');
-	const compiled = await compile(postContents);
-	const fm = compiled?.data?.fm as ArticleFrontmatter;
+export async function getArticleFrontmatter(
+	postPath: string,
+	postCompiled: MdsvexCompiled | undefined = undefined
+): Promise<ArticleFrontmatter> {
+	if (!postCompiled) {
+		const postContents = fs.readFileSync(postPath, 'utf-8');
+		postCompiled = await compile(postContents);
+	}
+	const fm = postCompiled?.data?.fm as ArticleFrontmatter;
 	
 	if (!fm.last_modified_at) {
 		try {
