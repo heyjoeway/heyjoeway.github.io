68a66d278fab189efce2087cfc5a1acdeb17744f
heyjoeway <joe@jojudge.com>
2025-06-30 11:02:42 -0400

    Disable ssr, generally not needed

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,7 @@
 import { getAllCategories } from '$lib/Article';
 
 export const prerender = true;
+export const ssr = false;
 export const trailingSlash = 'always';
 
 export async function load() {
