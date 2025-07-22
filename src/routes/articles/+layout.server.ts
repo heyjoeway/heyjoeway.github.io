ac99b97a80b21bec2be4c2d27c93c4f3ba81c860
heyjoeway <joe@jojudge.com>
2025-07-21 22:08:11 -0400

    Fix missing typing in article layout

--- src/routes/articles/+layout.server.ts
+++ src/routes/articles/+layout.server.ts
@@ -1,6 +1,6 @@
 import { getArticleFrontmatter } from '$lib/Article.js';
 
-export async function load({ route }) {
+export async function load({ route }: { route: { id: string } }) {
 	const postPath = `src/routes/${route.id}/+page.svx`;
 	const fm = await getArticleFrontmatter(postPath);
 	
